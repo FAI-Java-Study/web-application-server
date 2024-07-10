a5f886416d23fec614e7b29dba1e1b95f2e55e92
birdie <flaehdan@gmail.com>
2024-07-11 08:58:05 +0900

    feat : 회원가입 구현

---
+++ src/main/java/database/Database.java
@@ -0,0 +1,20 @@
+package database;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.User;
+
+public class Database {
+
+    private static Map<String, User> users = new HashMap<>();
+
+    public static void addUser(User user) {
+
+        users.put(user.getUserId(), user);
+    }
+
+    public static User getUser(String userId) {
+
+        return users.get(userId);
+    }
+}
