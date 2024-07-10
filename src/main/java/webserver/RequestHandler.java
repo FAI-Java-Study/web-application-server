f897489e090a266b7278c2ea51d14a70a13d9ac2
birdie <flaehdan@gmail.com>
2024-07-10 16:52:17 +0900

    feat : 1단계 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,18 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,10 +26,22 @@ public class RequestHandler extends Thread {
 	public void run() {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
-		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+		try (InputStream in = connection.getInputStream();
+			OutputStream out = connection.getOutputStream()) {
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+			String line = br.readLine();
+
+
+			if (line == null) {
+				return;
+			}
+
+			String path = HttpRequestUtils.getUrl(line);
+
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+			byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
