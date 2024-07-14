324bd378f8db5dbb96c8ec6c67f33d7ce16b838d
kuhi <tkddls4456@naver.com>
2024-07-14 21:52:59 +0900

    jar 실행을 위한 docker container 환경 설정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -10,7 +10,7 @@ public class WebServer {
 	private static final Logger log = LoggerFactory.getLogger(WebServer.class);
 	private static final int DEFAULT_PORT = 8080;
 	
-    public static void main(String argv[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
     	
     	try (ServerSocket listenSocket = new ServerSocket(DEFAULT_PORT)) {
