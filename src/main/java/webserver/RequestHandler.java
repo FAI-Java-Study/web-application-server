e65d15cd03a693485170bdb7f16230142da3b02f
kuhi <tkddls4456@naver.com>
2024-07-16 01:29:50 +0900

    3-4-2 get 요청으로 회원가입 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import static util.HttpRequestUtils.*;
 import static util.HttpRequestUtils.Pair.*;
 import static util.IOUtils.*;
 
@@ -10,14 +11,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
+
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-	private static final String INDEX_HTML = "/index.html";
+	private static final List<String> WEB_APP_ROUTE = Collections.unmodifiableList(
+		Arrays.asList("/index.html", "/form.html", "/login.html"));
+
+	private static final String CREATE_ROUTE = "/create?";
 
 	private final Socket connection;
 
@@ -39,8 +50,13 @@ public class RequestHandler extends Thread {
 
 			while (line != null && !"".equals(line)) {
 				requestedUrl = getRequestedUrl(line);
+				if (Objects.requireNonNull(requestedUrl).startsWith(CREATE_ROUTE)) {
+					User user = createUser(requestedUrl);
+					log.info("User: {}", user);
+					break;
+				}
 
-				if (INDEX_HTML.equals(requestedUrl)) {
+				if (WEB_APP_ROUTE.contains(requestedUrl)) {
 					break;
 				}
 				line = reader.readLine();
@@ -49,13 +65,34 @@ public class RequestHandler extends Thread {
 			byte[] body = generateResponseBody(requestedUrl);
 			response200Header(dos, body.length);
 			responseBody(dos, body);
-		} catch (IOException e) {
-			log.error(e.getMessage());
+		} catch (IOException ioException) {
+			log.error(ioException.getMessage());
+		} catch (Exception exception) {
+			log.error(exception.getMessage());
+			throw new RuntimeException("알수없는 error", exception);
 		}
 	}
 
+	/**
+	 * 요청정보를 통해 유저 데이터를 생성한다
+	 * @param requestedUrl
+	 * @return
+	 */
+	private User createUser(String requestedUrl) {
+		int index = requestedUrl.indexOf("?");
+		String queryString = requestedUrl.substring(index + 1);
+		Map<String, String> queryStringMap = parseQueryString(queryString);
+
+		String userId = queryStringMap.get("userId");
+		String password = queryStringMap.get("password");
+		String name = queryStringMap.get("name");
+		String email = queryStringMap.get("email");
+
+		return new User(userId, password, name, email);
+	}
+
 	private byte[] generateResponseBody(String requestedUrl) {
-		if (INDEX_HTML.equals(requestedUrl)) {
+		if (WEB_APP_ROUTE.contains(requestedUrl)) {
 			try {
 				return readFileAsByteArray(requestedUrl);
 			} catch (IOException e) {
