23a90faef7a1457857ec3261c9f1a77fe80505ae
kuhi <tkddls4456@naver.com>
2024-07-15 22:23:52 +0900

    3-1 http 요청 정보 line 별로 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -21,10 +22,20 @@ public class RequestHandler extends Thread {
 
 	public void run() {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
-		
+
 		try (InputStream in = connection.getInputStream();
-			 OutputStream out = connection.getOutputStream();) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			 OutputStream out = connection.getOutputStream();
+			 BufferedReader reader = new BufferedReader(new InputStreamReader(in));) {
+
+			String line = reader.readLine();
+
+			while (!"".equals(line)) {
+				if (line == null) {
+					return;
+				}
+				log.info("line: {}", line);
+				line = reader.readLine();
+			}
 
 			DataOutputStream dos = new DataOutputStream(out);
 			byte[] body = "Hello World".getBytes();
