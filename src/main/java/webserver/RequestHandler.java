247738800fb561882d411bb44cacaf363b742e55
kuhi <tkddls4456@naver.com>
2024-07-16 22:36:48 +0900

    3-4-3 post 요청으로 회원가입 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -27,8 +27,8 @@ public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 	private static final List<String> WEB_APP_ROUTE = Collections.unmodifiableList(
 		Arrays.asList("/index.html", "/form.html", "/login.html"));
-
-	private static final String CREATE_ROUTE = "/create?";
+	private static final String CREATE_ROUTE = "/create";
+	private static final String CONTENT_LENGTH = "Content-Length: ";
 
 	private final Socket connection;
 
@@ -48,10 +48,10 @@ public class RequestHandler extends Thread {
 			String line = reader.readLine();
 			String requestedUrl = "";
 
-			while (line != null && !"".equals(line)) {
+			while (line != null && !line.isEmpty()) {
 				requestedUrl = getRequestedUrl(line);
 				if (Objects.requireNonNull(requestedUrl).startsWith(CREATE_ROUTE)) {
-					User user = createUser(requestedUrl);
+					User user = createUser(reader);
 					log.info("User: {}", user);
 					break;
 				}
@@ -75,20 +75,35 @@ public class RequestHandler extends Thread {
 
 	/**
 	 * 요청정보를 통해 유저 데이터를 생성한다
-	 * @param requestedUrl
+	 * @param reader
 	 * @return
 	 */
-	private User createUser(String requestedUrl) {
-		int index = requestedUrl.indexOf("?");
-		String queryString = requestedUrl.substring(index + 1);
-		Map<String, String> queryStringMap = parseQueryString(queryString);
+	private User createUser(BufferedReader reader) throws IOException {
+		// header 읽기
+		String line = reader.readLine();
+		int contentLength = -1;
 
-		String userId = queryStringMap.get("userId");
-		String password = queryStringMap.get("password");
-		String name = queryStringMap.get("name");
-		String email = queryStringMap.get("email");
+		while (line != null && !line.isEmpty()) {
+			if (line.startsWith(CONTENT_LENGTH)) {
+				contentLength = Integer.parseInt(line.substring(CONTENT_LENGTH.length()));
+			}
+			line = reader.readLine();
+		}
 
-		return new User(userId, password, name, email);
+		// body 읽기
+		if (contentLength > 0) {
+			String bodyData = readData(reader, contentLength);
+
+			Map<String, String> queryStringMap = parseQueryString(bodyData);
+
+			String userId = queryStringMap.get("userId");
+			String password = queryStringMap.get("password");
+			String name = queryStringMap.get("name");
+			String email = queryStringMap.get("email");
+
+			return new User(userId, password, name, email);
+		}
+		return null;
 	}
 
 	private byte[] generateResponseBody(String requestedUrl) {
