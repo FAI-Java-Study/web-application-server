d5335fa2e8ba9d1e9807a70a4f12e9c2db2d0721
songks0922 <songks0922@gmail.com>
2024-07-17 21:12:38 +0900

    Feat: 요구사항 2번 구현 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,8 +10,11 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 import java.nio.file.Files;
+import java.util.Map;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -44,6 +47,28 @@ public class RequestHandler extends Thread {
 				if (tokens.length > 2 && "/index.html".equals(tokens[1])) {
 					body = Files.readAllBytes(new File("webapp" + tokens[1]).toPath());
 				}
+
+				if (tokens.length > 2 && tokens[1].contains("create")) {
+
+					int index = tokens[1].indexOf("?");
+
+					String requestPath = tokens[1].substring(0, index);
+					String params = tokens[1].substring(index + 1);
+
+					Map<String, String> stringStringMap = HttpRequestUtils.parseQueryString(params);
+
+					String email = stringStringMap.get("email");
+					String name = stringStringMap.get("name");
+					String userId = stringStringMap.get("userId");
+					String password = stringStringMap.get("password");
+
+					log.debug("email : {}, name : {}, userId : {}, password : {}", email, name, userId, password);
+
+					User user = new User(userId, password, name, email);
+
+					log.debug("user : {}", user);
+				}
+
 
 				line = br.readLine();
 
